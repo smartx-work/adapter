cd7c139df35482036bbb1c6e224959c28c868f62
shushu.c <chenshushu@meeruu.com>
2019-06-28 21:00:56 +0800

    扩展$format指令功能,支持传递函数类型做临时操作

--- tests/basic.test.js
+++ tests/basic.test.js
@@ -176,6 +176,14 @@ describe('\n=== 添加格式化指令 $format指令  ===', () => {
         expect(newData.timestamp).toBe(String(now))
     })
 
+    test('字符型配置简化模式；调用格式化指令', () => {
+        const newData = transform({
+            timestamp: '#dateFormat',
+        }, baseData)
+
+        expect(newData.timestamp).toBe(String(now))
+    })
+
     test('添加多条格化指令', () => {
         adapter.addFormater({
             prependYuan: (value) => '￥' + value,
@@ -199,7 +207,7 @@ describe('\n=== 添加格式化指令 $format指令  ===', () => {
         })
         const newData = transform({
             name: { $format: 'appendUnit:同学,很厉害' },
-            price: 'format:appendUnit:元,很贵',
+            price: '#appendUnit:元,很贵',
         }, baseData)
         expect(newData.name).toEqual(baseData.name + '同学很厉害')
         expect(newData.price).toEqual(baseData.price + '元很贵')
@@ -221,12 +229,32 @@ describe('\n=== 添加格式化指令 $format指令  ===', () => {
                 },
             },
             china: {
-                $format: ['addChildren', ['浙江', '北京', '上海']],
+                $format: { name: 'addChildren', args: [['浙江', '北京', '上海']] },
             },
         }, testData)
 
         expect(newData.proince).toBe('浙江:' + ['杭州', '丽水', '温州'])
         expect(newData.china).toBe('中国:' + ['浙江', '北京', '上海'])
+    })
+
+    test('多次调用指令', () => {
+        adapter.addFormater('increase', (value, num = 1) => { // 一个累加器，可以给一个值进行累加操作
+            return value + Number(num)
+        })
+        adapter.addFormater('toArray', (value, token = ',') => { // 转换数组
+            return value.split(token)
+        })
+
+        const testData = { value: 1 }
+        const newData = adapter.transform({ // 传递多条指令，就像管道函数一样，前面的结果作为后面的输入
+            value: {
+                $format: ['increase:1', 'increase:-2', 'increase:1234', (value) => String(value), 'toArray:'],
+            },
+        }, testData)
+
+        expect(newData).toEqual({
+            value: ['1', '2', '3', '4'],
+        })
     })
 })
 
@@ -710,6 +738,156 @@ describe('\n=== 非严格模式开启扁平化适配器 ===', () => {
                     price: '999',
                 },
             },
+        })
+    })
+})
+
+describe('\n=== 示例测试 ===', () => {
+    test('=== 基础示例', () => {
+        const originData = {
+            user: '张三',
+            sex: 0,
+            age: '18',
+            books: [
+                { name: '水浒传', type: 'm1', price: 50 },
+                { name: '西游记', type: 'm2', price: 60 },
+                { name: '三国演义', type: 'm3', price: null },
+            ],
+            address: ['浙江省', '杭州市', '江干区', '火车东站旁'],
+        }
+        const newData = adapter.transform({
+            user: 'name',
+            sex: { $enum: ['先生', '女士', '保密'] },
+            age: Number,
+            books: {
+                name: true,
+                type: { $emap: { m1: '武侠小说', m2: '神话小说', m3: '历史小说' } },
+                price: { $default: '未知', $value: (value) => '￥' + value.toFixed(2) },
+            },
+            address: (value) => value.join(''),
+        }, originData)
+
+        expect(newData).toEqual({
+            name: '张三',
+            sex: '先生',
+            age: 18,
+            books: [
+                { name: '水浒传', type: '武侠小说', price: '￥50.00' },
+                { name: '西游记', type: '神话小说', price: '￥60.00' },
+                { name: '三国演义', type: '历史小说', price: '未知' },
+            ],
+            address: '浙江省杭州市江干区火车东站旁',
+        })
+    })
+
+    test('=== 常规示例 ===', () => {
+        adapter.addFormater('dateDefault', (value, format) => { // 添加预设的格式化规则，后期可以快速调用
+            value = new Date(value)
+            if (format === 'YYYY') {
+                return value.getFullYear()
+            }
+            return value.getFullYear() + '/' + (value.getMonth() + 1) + '/' + value.getDate()
+        })
+
+        const originData = {
+            data1: [
+                {
+                    goodsCode: 'SP10001',
+                    goodsTitle: '一件神奇的衣服',
+                    price: 1.2,
+                    goodsType: 'normal',
+                    goodsStatus: 1,
+                    goodsSkuList: [
+                        {
+                            id: 1000,
+                            attrs: ['红色', 'XXL'],
+                        },
+                        {
+                            id: 1001,
+                            attrs: ['黑色', 'XXL'],
+                        },
+                    ],
+                    isSale: 1,
+                    createTime: 1561750763712,
+                    modifyTime: null,
+                },
+                {
+                    goodsCode: 'SP10002',
+                    goodsTitle: '一条神奇的裤子',
+                    price: 2.2,
+                    goodsType: 'virtual',
+                    goodsStatus: 2,
+                    goodsSkuList: [
+                        {
+                            id: 1000,
+                            attrs: ['红色', 'XXL'],
+                        },
+                        {
+                            id: 1001,
+                            attrs: ['黑色', 'XXL'],
+                        },
+                    ],
+                    isSale: 0,
+                    createTime: 1561750773712,
+                    modifyTime: 1561750773712,
+                },
+            ],
+            discardField1: null,
+            discardField2: null,
+            provinces: '中国,美国,日本',
+        }
+
+        const newData = adapter.transform({
+            data1: {
+                $key: 'list', // 重命名data1 => list
+                goodsCode: 'code', // 重命名goodsCode => code
+                goodsTitle: {
+                    $key: 'title',
+                    $value: (value) => '标题：' + value, // 进行重命名和数据格式化，可以通过扩展来生成速写指令`key:title #prepend:标题：`
+                },
+                price: (value) => '￥' + value.toFixed(2), // 进行数据格式
+                goodsType: { $emap: { normal: '常规商品', virtual: '虚拟商品' } }, // 进行映射转化，速写指令`emap:normal:常规商品,virtual:虚拟商品`
+                goodsStatus: { $enum: [null, '销售中', '已下架', '缺货'] }, // 进行枚举转化，速写指令`enum:,销售中,已下架,缺货`
+                goodsSkuList: (value) => value.map(item => item.attrs.join('-')), // 对数组的值直接处理
+                isSale: Boolean, // 类型
+                createTime: '#dateDefault', // 无参数预设格式化处理
+                modifyTime: { $default: '无', $format: 'dateDefault:YYYY' }, // 速写指令`default:无 #dateDefault:YYYY`
+            },
+            provinces: (value) => value.split(','), // 转换成数组，可以通过扩展生成速写指令`#toArray`,`{$format:{name:toArray, args:[/,\s*/]}}`
+        }, originData)
+
+        expect(newData).toEqual({
+            list: [
+                {
+                    code: 'SP10001',
+                    title: '标题：一件神奇的衣服',
+                    price: '￥1.20',
+                    goodsType: '常规商品',
+                    goodsStatus: '销售中',
+                    goodsSkuList: [
+                        '红色-XXL',
+                        '黑色-XXL',
+                    ],
+                    isSale: true,
+                    createTime: '2019/6/29',
+                    modifyTime: '无',
+                },
+                {
+                    code: 'SP10002',
+                    title: '标题：一条神奇的裤子',
+                    price: '￥2.20',
+                    goodsType: '虚拟商品',
+                    goodsStatus: '已下架',
+                    goodsSkuList: [
+                        '红色-XXL',
+                        '黑色-XXL',
+                    ],
+                    isSale: false,
+                    createTime: '2019/6/29',
+                    modifyTime: 2019,
+                },
+            ],
+            provinces: ['中国', '美国', '日本'],
         })
     })
 })
